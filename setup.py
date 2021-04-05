b4699bafe66b78f3ef5afad0938d8dbb06a047b7
gandrieux <guillaume.andrieux89@gmail.com>
2021-04-05 17:44:17 +0100

    added structure and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pair_trading',
+    version='',
+    packages=['pair_trading'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
